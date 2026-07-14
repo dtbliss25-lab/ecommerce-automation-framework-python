0498a0fc325b1135826ba0ad7e9c3cc66ec7aed9
dtbliss25-lab <dtbliss25@gmail.com>
2026-07-14 11:07:25 +1000

    add allure reports

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -27,3 +28,18 @@ def driver():
 @pytest.fixture
 def users_data():
     return read_json("test_data/users.json")
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver")
+
+        if driver:
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name="Failure Screenshot",
+                attachment_type=allure.attachment_type.PNG
+            )
