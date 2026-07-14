7a0fe0cd68ac0c73f9d4eb4a05a6e539765612bb
dtbliss25-lab <dtbliss25@gmail.com>
2026-07-14 12:07:03 +1000

    added chrome in requirements.txt

--- conftest.py
+++ conftest.py
@@ -11,19 +11,22 @@ from utils.data_reader import read_json
 @pytest.fixture
 def driver():
     options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
     options.add_experimental_option("prefs", {
     "credentials_enable_service": False,
     "profile.password_manager_enabled": False,
     "profile.password_manager_leak_detection": False
 })
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    
+
+
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install())
     )
-    driver.maximize_window()
+
 
     yield driver
 
